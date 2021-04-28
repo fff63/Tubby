ab64d4a136e0486c9878a866660d06628aa6e5c4
IREEN lusien <>
2021-04-28 23:10:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function open(){
+    let modal = document.getElementById('modal').classList.add('show');
+}
+
+function close(){
+    let modal = document.getElementById('modal').classList.add('hide');
+}
